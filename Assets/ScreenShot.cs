e7a1ae0ef1a9ee40f350a6c8b18b0d6a401e3e19
Steff Kempink <mswf@live.nl>
2015-10-26 17:08:33 +0100

    *screenshot shain

---
+++ Assets/ScreenShot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			Debug.Log("hai(");
+			Application.CaptureScreenshot("Screenshot2.png", 2);
+
+		}
+	}
+}
